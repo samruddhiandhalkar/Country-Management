121e45502051a9084cd0fc7230e4c6b0d0d7e8c4
samruddhiandhalkar <samruddhiandhalkar2007@gmail.com>
2025-07-14 20:35:40 +0530

    Update CountryDetails.java

--- src/main/java/ShowDetails/CountryDetails.java
+++ src/main/java/ShowDetails/CountryDetails.java
@@ -1,90 +1,95 @@
 package ShowDetails;
 
 import java.util.List;
-
 import App.HibernateUtil;
-import entity.CountryClass;
+import Entity1.CountryClass;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
-public class CountryDetails 
+public class CountryDetails
 {
-	public void showCountry(CountryClass country)
-	{
-		EntityTransaction tra;
-		try(EntityManager entityManager =HibernateUtil.getEntityManagerFactory().createEntityManager();)
-		{
-			tra=entityManager.getTransaction();
-			entityManager.persist(country);
-			tra.commit();
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-			}
-	}
-	
-	public List<CountryClass> getAllEmployee()
-	{
-		try(EntityManager entityManager =HibernateUtil.getEntityManagerFactory().createEntityManager();)
-		{
-			return entityManager.createQuery("from CountryClass",CountryClass.class).getResultList();
-		}
-	}
-	
-	
-	public void updateCountry(int updateId,String new_country_capital) 
-	{
-		EntityTransaction transcation=null;
-		
-		try(EntityManager entityManager =HibernateUtil.getEntityManagerFactory().createEntityManager();)
-		{
-			transcation=entityManager.getTransaction();
-			CountryClass c1= entityManager.find(CountryClass.class,updateId);
-			if(c1!=null)
-			{
-				c1.setCountry_capital(new_country_capital);
-				entityManager.merge(c1);
-	            System.out.println("Capital updated.");
-	        } 
-			else
-			{
-	            System.out.println(" Country not found.");
-	        }
-			
-			transcation.commit();
-		}
-		catch(Exception ee) 
-		{
-			ee.printStackTrace();
-		}
-	}
-	
-	public void delConuntry(int deleteId)
-	{
-		EntityTransaction transcation=null;
-		
-		try(EntityManager entityManager =HibernateUtil.getEntityManagerFactory().createEntityManager();)
-		{
-			transcation=entityManager.getTransaction();
-			CountryClass country=entityManager.find(CountryClass.class,deleteId);
-			if(country!=null)
-			{
-				entityManager.remove(country);
-				System.out.println("Country can be remove");
-			}
-				else
-				{
-					System.out.println("Country cannot be found");
-				}
-			
-			transcation.commit();
-		}
-		catch(Exception ee)
-		{
-			ee.printStackTrace();
-		}
-	}
-	
 
+    
+    public void showCountry(CountryClass country) 
+    {
+        EntityTransaction tra = null;
+        try (EntityManager entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager()) 
+        {
+            tra = entityManager.getTransaction();
+            tra.begin(); 
+            entityManager.persist(country);  
+
+            tra.commit();  
+            System.out.println("Country inserted successfully.");
+        }
+        catch (Exception e)
+        {
+            if (tra != null && tra.isActive()) tra.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    
+    public List<CountryClass> getAll() 
+    {
+        try (EntityManager entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager())
+        {
+            return entityManager.createQuery("from CountryClass", CountryClass.class).getResultList();
+        }
+    }
+
+    
+    public void updateCountry(int updateId, String new_country_capital) 
+    {
+        EntityTransaction tx = null;
+        try (EntityManager entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager()) 
+        {
+            tx = entityManager.getTransaction();
+            tx.begin();
+
+            CountryClass country = entityManager.find(CountryClass.class, updateId);
+            if (country != null) 
+            {
+                country.setCountry_capital(new_country_capital);
+                entityManager.merge(country);
+                System.out.println("Capital updated.");
+            } else {
+                System.out.println("Country not found.");
+            }
+
+            tx.commit();
+        }
+        catch (Exception e) 
+        {
+            if (tx != null && tx.isActive()) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+   
+    public void delConuntry(int deleteId)
+    {
+        EntityTransaction tx = null;
+        try (EntityManager entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager()) 
+        {
+            tx = entityManager.getTransaction();
+            tx.begin();
+
+            CountryClass country = entityManager.find(CountryClass.class, deleteId);
+            if (country != null)
+            {
+                entityManager.remove(country);
+                System.out.println("✅ Country removed.");
+            } else {
+                System.out.println("❌ Country not found.");
+            }
+
+            tx.commit();
+        }
+        catch (Exception e)
+        {
+            if (tx != null && tx.isActive()) tx.rollback();
+            e.printStackTrace();
+        }
+    }
 }
